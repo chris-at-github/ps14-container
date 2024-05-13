724334f3afb3858c2e488a0cb18d9ca971c5e5f1
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-13 23:36:00 +0200

    Übernahme TypoScript, Templates und Settings aus ursprünglicher Extension

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,7 +3,7 @@
 \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
 	(
 	new \B13\Container\Tca\ContainerConfiguration(
-		'ps14_container', // CType
+		'ps14_container_2_column', // CType
 		'LLL:EXT:ps14_container/Resources/Private/Language/locallang_tca.xlf:two-column.title', // label
 		'LLL:EXT:ps14_container/Resources/Private/Language/locallang_tca.xlf:two-column.description', // description
 		[
@@ -17,3 +17,12 @@
 		// set an optional icon configuration
 		->setIcon('EXT:ps14_container/Resources/Public/Icons/ps14-module-two-column.svg')
 );
+
+// Flexform
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+	'*',
+	'FILE:EXT:ps14_container/Configuration/FlexForms/ContainerColumns.xml',
+	'ps14_container_2_column'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'pi_flexform', 'ps14_container_2_column', 'after:header');
